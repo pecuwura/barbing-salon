b67bdd2b032fcbb9a6e8b1f6239256443b133ac0
odeyemi peculiar <odeyemipeculiar@gmail.com>
2022-08-20 20:31:38 +0100

    My project

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const navs = document.getElementById("nav-links");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+let swiperPortfolio = new Swiper(".testimonials-container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
